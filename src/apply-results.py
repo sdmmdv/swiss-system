27e09a34025d7efc8b183cf0a4acfc4d521d2dba
sdmmdv <sadimamedov7@gmail.com>
2023-05-27 22:46:33 +0200

    Buchholz system set as first tiebraker

--- src/apply-results.py
+++ src/apply-results.py
@@ -39,6 +39,64 @@ def apply_scores_to_standings(conn, round_id):
         print(f"Error: {str(e)}")
         sys.exit(1)
 
+def apply_buchholz_tiebreak(conn):
+    try:
+        # Retrieve the list of players from the standings table
+        with conn.cursor() as cur:
+            cur.execute("SELECT id, name FROM standings")
+            players = cur.fetchall()
+            # print(players)
+
+            # Get average points of all players
+            cur.execute("SELECT AVG(points) AS average_points FROM standings")
+            result = cur.fetchone()
+            average_points = format(result[0], '.2f')
+
+            # Update the Buchholz tie-breaker for each player
+            for player in players:
+                player_id = player[0]
+                cur.execute("""
+                    SELECT 
+                        CASE 
+                            WHEN player1_id = %s THEN player2_id
+                            WHEN player2_id = %s THEN player1_id
+                        END AS opponent_id,
+                        CASE
+                            WHEN player1_id = %s THEN player2_score
+                            WHEN player2_id = %s THEN player1_score
+                        END AS opponent_score
+                    FROM results
+                    WHERE player1_id = %s OR player2_id = %s;
+                """, ([player_id] * 6))
+
+                # Fetch all the opponent results
+                opponent_data = cur.fetchall()
+
+                # opponent_ids = [result[0] for result in opponent_data]
+                # opponent_points = [result[1] for result in opponent_data]
+                buchholz_score_sum = 0
+                for data in opponent_data:
+                    opponent_id = data[0]
+                    opponent_point = data[1]
+                    # buchholz score of BYE opponent should be set to average of all players
+                    if opponent_id == '_':
+                        opponent_point = average_points
+                    buchholz_score_sum += float(opponent_point)
+
+                try:
+                    cur.execute("UPDATE standings SET tiebreaker_a = %s WHERE id = %s", (buchholz_score_sum, player_id))
+                except psycopg2.Error as e:
+                    conn.rollback()
+                    print(f"Error: {str(e)}")
+                    sys.exit(1)
+            # Commit the changes to the database
+            conn.commit()
+            print("Buchholz applied successfully")
+
+    except psycopg2.Error as e:
+        conn.rollback()
+        print(f"Error: {str(e)}")
+        sys.exit(1)
 
 def update_standings_table(conn):
     try:
@@ -64,7 +122,6 @@ def update_standings_table(conn):
         conn.rollback()
         print(f"Error: {str(e)}")
         sys.exit(1)
-
 
 def print_standings(conn):
     try:
@@ -118,6 +175,9 @@ if __name__ == '__main__':
 
     # Apply scores to standings
     # apply_scores_to_standings(conn, args.round_id)
+
+    # Apply buchholz_tiebreak
+    apply_buchholz_tiebreak(conn)
 
     # Update standings table
     update_standings_table(conn)
