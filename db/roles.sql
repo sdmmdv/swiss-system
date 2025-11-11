43208b5af9bd15b6eeddd00d20353dd6210a9aeb
Sadi Mamedov <sadimamedov7@gmail.com>
2025-11-11 17:47:32 +0100

    To keep SQL idempotency meaning it can run safely even if the user or database already exists.
    * here rewritten rules safely

--- db/roles.sql
+++ db/roles.sql
@@ -3,15 +3,31 @@
 -- #############################################################################################
 
 -- Admin with full control
-DROP ROLE IF EXISTS app_admin;
-CREATE ROLE app_admin WITH LOGIN PASSWORD 'admin_secure_pass' CREATEDB CREATEROLE;
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'app_admin') THEN
+       CREATE ROLE app_admin WITH LOGIN PASSWORD 'admin_secure_pass' CREATEDB CREATEROLE;
+   END IF;
+END
+$$;
 
--- Guest with read only
-DROP ROLE IF EXISTS app_guest;
-CREATE ROLE app_guest WITH LOGIN PASSWORD 'guest_pass';
+-- Guest with read-only
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'app_guest') THEN
+       CREATE ROLE app_guest WITH LOGIN PASSWORD 'guest_pass';
+   END IF;
+END
+$$;
 
-DROP DATABASE IF EXISTS tournament;
-CREATE DATABASE tournament;
+-- Database
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'tournament') THEN
+       CREATE DATABASE tournament;
+   END IF;
+END
+$$;
 
 -- #############################################################################################
 -- #############################################################################################
